ef4a9fda96c5eb34c9f48f334cf22c2a2b6e6a27
victord <victordaniel.123rt@gmail.com>
2026-08-20 13:19:56 -0600

    Commit

---
+++ src/main/java/com/vdgarcia/Hexagon_Migration_Service/HexagonMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vdgarcia.Hexagon_Migration_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonMigrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonMigrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdgarcia/Hexagon_Migration_Service/HexagonMigrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdgarcia.Hexagon_Migration_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonMigrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
