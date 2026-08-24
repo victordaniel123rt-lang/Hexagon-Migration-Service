e9e260501ca789510283979979a1b82692e5c915
victord <victordaniel.123rt@gmail.com>
2026-08-24 10:39:31 -0600

    Application-Service

---
+++ src/main/java/com/vdgarcia/Hexagon_Migration_Service/controller/ClienteController.java
@@ -0,0 +1,8 @@
+package com.vdgarcia.Hexagon_Migration_Service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ClienteController {
+}
