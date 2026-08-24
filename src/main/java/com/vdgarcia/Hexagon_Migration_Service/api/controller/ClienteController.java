b0e7461c156d336d01b4f67f080cfef70a8420ad
victord <victordaniel.123rt@gmail.com>
2026-08-24 12:00:15 -0600

    api-Service

--- src/main/java/com/vdgarcia/Hexagon_Migration_Service/api/controller/ClienteController.java
+++ src/main/java/com/vdgarcia/Hexagon_Migration_Service/api/controller/ClienteController.java
@@ -1,7 +1,54 @@
 package com.vdgarcia.Hexagon_Migration_Service.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.vdgarcia.Hexagon_Migration_Service.api.dto.ClienteDTO;
+import com.vdgarcia.Hexagon_Migration_Service.api.mapper.Mapper;
+import com.vdgarcia.Hexagon_Migration_Service.application.service.ClienteService;
+import com.vdgarcia.Hexagon_Migration_Service.domain.model.Cliente;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/cliente")
 public class ClienteController {
+
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> obtenerTodos(){
+        return ResponseEntity.ok(service.obtenerTodos().stream().map(Mapper::toClienteDTO).toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDTO> obtenerPorId(@PathVariable Long id){
+        ClienteDTO dto = Mapper.toClienteDTO(service.obtenerId(id));
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> crear(@RequestBody Cliente model){
+        return ResponseEntity.ok(Mapper.toClienteDTO(service.crear(model)));
+    }
+
+    @PutMapping
+    public ResponseEntity<ClienteDTO> actualizar(@PathVariable Long id, @RequestBody Cliente model){
+        return ResponseEntity.ok(Mapper.toClienteDTO(service.actualizar(id,model)));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity eliminar(@PathVariable Long id){
+        service.eliminar(id);
+        return ResponseEntity.ok(HttpEntity.EMPTY);
+    }
+
+
+
+
+
 }
--- src/main/java/com/vdgarcia/Hexagon_Migration_Service/api/mapper/Mapper.java
+++ src/main/java/com/vdgarcia/Hexagon_Migration_Service/api/mapper/Mapper.java
@@ -1,4 +1,124 @@
 package com.vdgarcia.Hexagon_Migration_Service.api.mapper;
 
+import com.vdgarcia.Hexagon_Migration_Service.api.dto.ClienteDTO;
+import com.vdgarcia.Hexagon_Migration_Service.api.dto.DetallePedidoDTO;
+import com.vdgarcia.Hexagon_Migration_Service.api.dto.PedidoDTO;
+import com.vdgarcia.Hexagon_Migration_Service.api.dto.ProductoDTO;
+import com.vdgarcia.Hexagon_Migration_Service.domain.model.Cliente;
+import com.vdgarcia.Hexagon_Migration_Service.domain.model.DetallePedido;
+import com.vdgarcia.Hexagon_Migration_Service.domain.model.Pedido;
+import com.vdgarcia.Hexagon_Migration_Service.domain.model.Producto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mapper {
+
+    public static ClienteDTO toClienteDTO(Cliente cliente){
+        if (cliente==null) return null;
+        return ClienteDTO.builder()
+                .id(cliente.getId())
+                .nombre(cliente.getNombre())
+                .email(cliente.getEmail())
+                .apellido(cliente.getApellido())
+                .telefono(cliente.getTelefono())
+                .pedidos(new ArrayList<>())
+                .build();
+    }
+
+    public static PedidoDTO toPedidoDTO(Pedido pedido){
+        if (pedido == null) return null;
+        List<DetallePedidoDTO> detalles = pedido.getDetalles().stream().map(Mapper::toDetallePedidoDTO).toList();
+        return PedidoDTO.builder()
+                .id(pedido.getId())
+                .fecha(pedido.getFecha())
+                .total(pedido.getTotal())
+                .estado(pedido.getEstado())
+                .cliente(pedido.getCliente().getId())
+                .detalles(detalles)
+                .build();
+    }
+
+
+    public static DetallePedidoDTO toDetallePedidoDTO(DetallePedido detalle){
+        if (detalle == null) return null;
+        return DetallePedidoDTO.builder()
+                .id(detalle.getId())
+                .pedido(detalle.getPedido().getId())
+                .producto(detalle.getProducto().getId())
+                .cantidad(detalle.getCantidad())
+                .build();
+
+    }
+
+
+
+    public static ProductoDTO toProductoDTO(Producto producto){
+        if (producto==null) return null;
+        List<DetallePedidoDTO> detalles = producto.getDetalles().stream().map(Mapper::toDetallePedidoDTO).toList();
+        return ProductoDTO.builder()
+                .id(producto.getId())
+                .nombre(producto.getNombre())
+                .stock(producto.getStock())
+                .descripcion(producto.getDescripcion())
+                .precio(producto.getPrecio())
+                .detalles(detalles)
+                .build();
+    }
+
+
+    public static Cliente toCliente(ClienteDTO dto){
+        if(dto == null) return null;
+        List<Pedido> pedidos = dto.getPedidos().stream().map(Mapper::toPedido).toList();
+        return new Cliente(
+                dto.getApellido(),
+                dto.getEmail(),
+                dto.getId(),
+                dto.getNombre(),
+                pedidos,
+                dto.getTelefono()
+        );
+    }
+
+
+    public static Pedido toPedido(PedidoDTO dto){
+        if (dto==null) return null;
+        List<DetallePedido> detalles = dto.getDetalles().stream().map(Mapper::toDetallePedido).toList();
+        return  new Pedido(
+                dto.getFecha(),
+                dto.getEstado(),
+                detalles,
+                new Cliente(dto.getId()),
+                dto.getTotal()
+        );
+    }
+
+    public static DetallePedido toDetallePedido(DetallePedidoDTO dto){
+        if (dto==null) return null;
+        return new DetallePedido(
+                dto.getCantidad(),
+                dto.getId(),
+                new Pedido(dto.getPedido()),
+                new Producto(dto.getProducto())
+        );
+    }
+
+
+    public static Producto toProducto(ProductoDTO dto){
+        if (dto == null) return null;
+        List<DetallePedido> detalles = dto.getDetalles().stream().map(Mapper::toDetallePedido).toList();
+        return new Producto(dto.getDescripcion(), detalles, dto.getId(), dto.getNombre(), dto.getPrecio(), dto.getStock()
+        );
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
